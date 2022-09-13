9526cee40e8b9e293ccc211e40d03661cba8a68a
Stéphane Travostino <steph@combo.cc>
2022-09-13 11:53:46 +0100

    .

--- scripts/generate-repo.sh
+++ scripts/generate-repo.sh
@@ -8,7 +8,13 @@ RECIPES_DIRECTORY="$ROOT/recipes"
 REPO_FILE="$ROOT/repo.yml"
 NEW_REPO_FILE="$REPO_FILE.new"
 
+if [ -x "$ROOT/fonz" ]; then
+    FONZ="$ROOT/fonz"
+else
+    FONZ="fonz"
+fi
+
 find "$RECIPES_DIRECTORY" -type f -name 'recipe.yml' -print0 | sort -z | xargs -0 cat > "$NEW_REPO_FILE"
-fonz validate-repo "$NEW_REPO_FILE" >/dev/null
+$FONZ validate-repo "$NEW_REPO_FILE" >/dev/null
 cp -f "$NEW_REPO_FILE" "$REPO_FILE"
 rm -f "$NEW_REPO_FILE"
